4e34794ccc62dff91b36e0e8ccfaa3b8c6cf01d6
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2023-12-13 22:41:11 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,45 @@
+'use client';
+import styles from './page.module.scss'
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+import Preloader from '../components/Preloader/index';
+import Landing from '../components/Landing/index';
+import Projects from '../components/Projects/index';
+import Description from '../components/Description/index';
+import SlidingImages from '../components/SlidingImages/index';
+import Contact from '../components/Contact/index';
+import RootLayout from './layout';
+
+export default function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
+
+  return (
+    <main className={styles.main}>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+      <RootLayout/>
+      <Landing />
+      <Description />
+      <Projects />
+      <SlidingImages />
+      <Contact />
+    </main>
+  )
+}
---
+++ src/components/Header/nav/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import styles from './style.module.scss';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
+import { menuSlide } from '../animation';
+import Link from './Link';
+import Curve from './Curve';
+import Footer from './Footer';
+
+const navItems = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Work",
+    href: "/work",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+]
+
+export default function index() {
+
+  const location = useLocation();
+  const [selectedIndicator, setSelectedIndicator] = useState(location);
+
+  return (
+    <motion.div 
+      variants={menuSlide} 
+      initial="initial" 
+      animate="enter" 
+      exit="exit" 
+      className={styles.menu}
+      >
+       <div className={styles.body}>
+            <div onMouseLeave={() => {setSelectedIndicator(location)}} className={styles.nav}>
+                    <div className={styles.header}>
+                        <p>Navigation</p>
+                    </div>
+                    {
+                      navItems.map( (data, index) => {
+                        return <Link 
+                        key={index} 
+                        data={{...data, index}} 
+                        isActive={selectedIndicator == data.href} 
+                        setSelectedIndicator={setSelectedIndicator}>
+                        </Link>
+                      })
+                    }
+            </div>
+            <Footer />
+        </div>
+        <Curve />
+    </motion.div>
+  )
+}
